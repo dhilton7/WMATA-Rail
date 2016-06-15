badff3330c2f1c86b7e5978dd319c6e96d840841
Dan Hilton <danhilton@Dans-MacBook-Pro.local>
2016-06-15 15:18:11 -0400

    updated wmataswift framework and added view controller with line options

---
+++ WMATA-Rail/LinesTableViewController.swift
@@ -0,0 +1,68 @@
+//
+//  LinesTableViewController.swift
+//  WMATA-Rail
+//
+//  Created by Dan Hilton on 6/15/16.
+//  Copyright © 2016 Dan Hilton. All rights reserved.
+//
+
+import UIKit
+import WMATASwift
+
+class LinesTableViewController: UITableViewController {
+
+    private var lines: [Line]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        self.getLines()
+        
+    }
+
+    private func getLines() {
+        Rail.sharedInstance.wrapper.getTrainLines({ (lines:[Line]) in
+            self.lines = lines
+            dispatch_async(dispatch_get_main_queue(), { 
+                self.tableView.reloadData()
+            })
+        }) { (error:NSError) in
+            
+        }
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.lines?.count ?? 0
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("lineCell", forIndexPath: indexPath)
+        cell.textLabel?.text = self.lines![indexPath.row].displayName
+
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
