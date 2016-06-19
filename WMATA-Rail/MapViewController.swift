a17224e899940504d0b93ff66372e180de3ee031
Dan Hilton <dhilton@missiondata.com>
2016-06-19 18:59:00 -0400

    hook up accesory button from map view

--- WMATA-Rail/MapViewController.swift
+++ WMATA-Rail/MapViewController.swift
@@ -17,6 +17,7 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     
     
     private var stations: [Station]?
+    private var stationAnnotations: [MetroAnnotation]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,36 +39,50 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     }
     
     private func updateMapUI() {
-        if stations != nil {
-            var annotations = [MKPointAnnotation]()
+        if stations != nil && stationAnnotations == nil {
+            stationAnnotations = [MetroAnnotation]()
             for s in stations! {
-                let annotation = MKPointAnnotation()
+                let annotation = MetroAnnotation()
                 annotation.coordinate = CLLocationCoordinate2D(latitude: s.latitude!, longitude: s.longitude!)
                 annotation.title = s.name
                 annotation.subtitle = s.lineCodes?.joinWithSeparator(", ")
-                annotations.append(annotation)
+                annotation.stationCode = s.code
+                self.stationAnnotations!.append(annotation)
             }
-            mapView.addAnnotations(annotations)
         }
+        mapView.addAnnotations(stationAnnotations!)
     }
     
     // MARK: - Map View Delegate
     func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
-        let reuseId = "StationPin"
-        var annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId)
-        if annotationView == nil {
-            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
-            annotationView?.canShowCallout = true
-            annotationView?.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure) as UIView
+        if let metroPoint = annotation as? MetroAnnotation {
+            let reuseId = "StationPin"
+            var annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId)
+            if annotationView == nil {
+                annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+                annotationView?.canShowCallout = true
+                annotationView?.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure) as UIView
+            }
+            else {
+                annotationView?.annotation = annotation
+            }
+            annotationView?.image = UIImage(named: metroPoint.imgName)
+            return annotationView
         }
         else {
-            annotationView?.annotation = annotation
+            return nil
         }
-        annotationView?.image = UIImage(named: "TrainIcon.png")
-        return annotationView
     }
     
-    
+    func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if let metroPoint = view.annotation as? MetroAnnotation {
+            if let controller = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("PredictionViewController") as? PredictionViewController {
+                controller.stationCode = metroPoint.stationCode
+                controller.stationName = metroPoint.title
+                self.navigationController?.pushViewController(controller, animated: true)
+            }
+        }
+    }
 
     
     
---
+++ WMATA-Rail/MetroAnnotation.swift
@@ -0,0 +1,16 @@
+//
+//  MetroAnnotation.swift
+//  WMATA-Rail
+//
+//  Created by Dan Hilton on 6/19/16.
+//  Copyright © 2016 Dan Hilton. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MetroAnnotation: MKPointAnnotation {
+
+    var stationCode: String?
+    let imgName = "TrainIcon.png"
+}
